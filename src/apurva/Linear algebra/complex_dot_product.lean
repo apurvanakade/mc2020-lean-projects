e3656697514aa6c99e989ba3f01be05ab1e45093
jalex-stark <alexmaplegm@gmail.com>
2020-07-19 19:47:15 -0400

    comments for apurva

--- src/apurva/Linear algebra/complex_dot_product.lean
+++ src/apurva/Linear algebra/complex_dot_product.lean
@@ -7,53 +7,64 @@ import tactic
 noncomputable theory 
 open_locale big_operators 
 
-universes u u'
+universes u v
 variables {n m : Type u} [fintype n] [fintype m]
 
 local notation `Euc` := (n → ℂ)
 
-instance hn_fd : finite_dimensional ℂ Euc := 
-begin 
-  apply_instance
-end 
+-- CR jstark for apurvnakade: 
+--  If `apply_instance` can provide the instance, then you don't need to declare it.
+
+-- instance hn_fd : finite_dimensional ℂ Euc := 
+-- begin 
+--   apply_instance
+-- end 
 
 open matrix
-  /-- Complex conjugate of a vector.-/
-  def conj (M : matrix m n ℂ) : matrix m n ℂ := 
-  λ i j, complex.conj (M i j)
+def complex_transpose (M : matrix m n ℂ) : matrix n m ℂ
+| x y := complex.conj (M y x)
 
-  def complex_transpose (M : matrix m n ℂ) : matrix n m ℂ
-  | x y := complex.conj (M y x)
+namespace vector
+/-- Complex conjugate of a vector.-/
+def conj (M : matrix m n ℂ) : matrix m n ℂ := 
+λ i j, complex.conj (M i j)
 
-  def complex_dot_product (v : Euc) (w : Euc) : ℂ := 
-  ∑ i, complex.conj (v i) * w i  
+def complex_dot_product (v : Euc) (w : Euc) : ℂ := 
+∑ i, complex.conj (v i) * w i  
 
-  def orthogonal (v : Euc) (w : Euc) : Prop := 
-  (complex_dot_product v w = 0)  
+def orthogonal (v : Euc) (w : Euc) : Prop := 
+(complex_dot_product v w = 0)  
 
-  def orthogonal_complement (v : Euc) : subspace ℂ Euc := 
-  { carrier := sorry,
+def orthogonal_complement (v : Euc) : subspace ℂ Euc := 
+{ carrier := sorry,
   zero_mem' := sorry,
   add_mem' := sorry,
   smul_mem' := sorry }
 
-end matrix
+end vector
+-- CR jstark for apurvnakade: `open matrix` just makes things in the matrix namespace available.
+--  there is no `end` to pair with `open`. You do pair `end` with `namespace` and with `section`.
+
+-- end matrix
 
 namespace subspace 
   -- variables {W : Type u'} [add_comm_group W] [vector_space ℂ W] [finite_dimensional ℂ W]
 
-  def is_orthogonal_subspace (S : set Euc) : subspace ℂ Euc → Prop := 
-  begin 
-    intro W,
-    sorry,
-  end 
+def is_orthogonal_subspace (S : set Euc) : subspace ℂ Euc → Prop := 
+begin 
+  intro W,
+  sorry,
+end 
 
-  def orthogonal_complement (S : set Euc) : subspace ℂ Euc := 
-  sorry -- infimum of all 
+def orthogonal_complement (S : set Euc) : subspace ℂ Euc := 
+Inf {U | ∀ v ∈ S, v ∈ U}
 
-  -- need coercion from subspace to larger space
-  theorem orthogonal_in_orthogonal_complement (S : set Euc) (v : subspace ℂ Euc) (w : orthogonal_complement S) : 
-    vector.orthogonal (v : Euc) (w : Euc) := 
-  sorry
+-- CR jstark for apurvnakade: I don't understand what your theorem is supposed to say.
+--  Is this version what you were going for?
+-- need coercion from subspace to larger space
+theorem orthogonal_in_orthogonal_complement 
+(S : set Euc) {v w : Euc} (hv : v ∈ S) (hw : w ∈ orthogonal_complement S) : 
+  vector.orthogonal v w := 
+sorry
 
 end subspace
