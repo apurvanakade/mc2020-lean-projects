c51fa574d842fd3296c15122e53a12070d102204
jalex-stark <alexmaplegm@gmail.com>
2020-07-28 20:22:11 -0400

    trying new path definition

--- src/michael/path.lean
+++ src/michael/path.lean
@@ -10,39 +10,50 @@ variables (G : simple_graph V)
 -- Then we'd have more lemmas available and `mem_cons` would be ~just an application of of the same for lists. 
 /-- A path is an ordered list of vertices where consecutive elements are adjacent. -/
 inductive path : V → V → Type u
-| nil  : Π (h : V), path h h
-| cons : Π {h s t : V} (e : G.adj h s) (l : path s t), path h t
+| nil  : Π (v : V), path v v 
+| cons : Π {v s t : V} (e : G.E) (hv : v ∈ e) (hs : s ∈ e) (l : path s t), path v t
 
-notation a :: b := path.cons a b
+-- notation a :: b := path.cons a b
+namespace path
+variables {G}
+/-- For e an edge and p a path, G.mem e p is true if the edge lies on the path. -/
+inductive mem : Π {s t : V} (e : G.E) (p : G.path s t), Prop
+| nil : ∀ {v} (e : G.E) (h : false), mem e (path.nil v)
+| head : ∀ {v s t : V} (e : G.E) (hv : v ∈ e) (hs : s ∈ e) (tl : G.path v t), mem e (tl.cons e hs hv)
+| tail : ∀ {v s t : V} (e e' : G.E) (hv : v ∈ e) (hs : s ∈ e) (tl : G.path v t) (h : mem e' tl) , mem e' (tl.cons e hs hv)
+
+instance has_mem_edge {s t : V} : has_mem G.E (G.path s t) := 
+{ mem := mem }
 
 /-- For e an edge and p a path, G.mem e p is true if the edge lies on the path. -/
-inductive mem : Π {w x y z : V} (e : G.adj x y) (p : G.path w z), Prop
-| head : ∀ {x y z} (e : G.adj x y) (p : G.path y z), mem e (e :: p)
-| head_symm : ∀ {x y z} (e : G.adj x y) (p : G.path y z), mem (G.sym e) (e :: p)
-| tail : ∀ {v w x y z} (e : G.adj v w) (e' : G.adj x y) (p : G.path w z) (m : mem e' p), mem e' (e :: p)
+inductive vmem : Π {s t : V} (v : V) (p : G.path s t), Prop
+| nil : ∀ v, vmem v (path.nil v)
+| head : ∀ {v s t : V} (p : G.path s t), vmem s p
+| tail : ∀ {v s t : V} (e : G.E) (hv : v ∈ e) (hs : s ∈ e) (tl : G.path v t) (h : vmem v tl), vmem v (tl.cons e hs hv)
 
-inductive is_trail : Π {x y} (p : G.path x y), Prop
-| nil : ∀ (x), is_trail (path.nil x)
-| cons : ∀ {x y z} (e : G.adj x y) (p : G.path y z) (h : ¬ G.mem e p), is_trail (e :: p)
+instance has_mem_vertex {s t : V} : has_mem V (G.path s t) := 
+{ mem := vmem }
+
+inductive is_trail : Π {s t} (p : G.path s t), Prop
+| nil : ∀ v, is_trail (path.nil v)
+| tail : ∀ {v s t} (e : G.E) (hv : v ∈ e) (hs : s ∈ e) (tl : G.path v t) (h : ¬ tl.mem e), is_trail (tl.cons e hs hv)
 
 def is_Eulerian {x y} (p : G.path x y) : Prop :=
-G.is_trail p ∧ ∀ {x' y'} (e : G.adj x' y'), G.mem e p
+p.is_trail ∧ ∀ e, p.mem e
 
-lemma no_edge_in_nil {d x y : V} (h : G.adj x y) : ¬ G.mem h (path.nil d) :=
+lemma no_edge_in_nil {d x y : V} (e : G.E) : ¬ (path.nil d).mem e :=
 by rintro ⟨⟩
 
-@[simp] lemma path.mem_cons {x s t u v: V} (e : G.adj x s) (h : G.adj u v) (p : G.path s t) :
- G.mem h (e :: p) ↔ u = x ∧ v = s ∨ G.mem h p :=
+lemma path.mem_cons
+ {v s t : V} (e hd : G.E) (hv : v ∈ e) (hs : s ∈ e) (tl : G.path v t) :
+ (path.cons e hs hv tl).mem e ↔ e = hd ∨ tl.mem e :=
 begin
-  split,
-  intro m, 
-  by_cases eq : u = x ∧ v = s,
-  {left, exact eq},
-  right,
-  push_neg at eq,
-  induction m, tidy,
+  by_cases h : e = hd, { simp [h, mem.head] },
+  split, intro a, 
+  cases a,
   sorry,
   sorry,
 end
 
+end path
 end simple_graph
